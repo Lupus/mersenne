91c3ceb8c3acdb81436c792ce062db0394529b08
Konstantin Olkhovskiy <lupus@oxnull.net>
2012-06-30 13:45:18 +0400

    Simulation program now outputs measurements

--- simulation/simulate.c
+++ simulation/simulate.c
@@ -44,6 +44,9 @@
 #define LINE_BUF_SIZE 10 * 1024
 #define PROC_NUM 4
 
+#define PROC_LEADER_NOT_SURE	-1
+#define PROC_LEADER_UNDEFINED	-2
+
 
 struct child_proc_item {
 	int cpi_fd;
@@ -57,9 +60,12 @@ struct child_proc_item {
 };
 
 static struct child_proc_item *children;
-//static ev_timer kill_timer;
+static ev_timer sim_timer;
+static ev_timer kill_timer;
 static struct ev_loop *loop;
 static struct timeval start_tv;
+static int killed_children;
+static int current_leader;
 
 static regex_t rx_new_round;
 static regex_t rx_leader;
@@ -95,19 +101,49 @@ static float timeval_msec_delta(struct timeval *from, struct timeval *to)
 	return (u_to - u_from) / 1000.0;
 }
 
+static void clear_leaders()
+{
+	int i;
+	for(i = 0; i < PROC_NUM; i++) {
+		leaders[i] = PROC_LEADER_UNDEFINED;
+
+	}
+}
+
+static void dump_leaders()
+{
+	int i;
+	fprintf(stderr, "[");
+	for(i = 0; i < PROC_NUM; i++) {
+		fprintf(stderr, "%d", leaders[i]);
+		if(i < PROC_NUM - 1)
+			fprintf(stderr, ", ");
+
+	}
+	fprintf(stderr, "]\n");
+}
+
 static int check_leader_election()
 {
 	int i;
-	int leader;
+	int leader = PROC_LEADER_UNDEFINED;
+	int votes = 0;
 
-	assert(PROC_NUM > 1);
-	leader = leaders[0];
-	for(i = 1; i < PROC_NUM; i++) {
-		if(leaders[i] != leader)
+	for(i = 0; i < PROC_NUM; i++) {
+		if(leaders[i] == PROC_LEADER_UNDEFINED)
+			continue;
+		if(leader == PROC_LEADER_UNDEFINED)
+			leader = leaders[i];
+		else if(leaders[i] != leader)
 			return 0;
+		votes++;
 	}
+	fprintf(stderr, "*** %d\n", leader);
 	if(leader < 0)
 		return 0;
+	if(votes < PROC_NUM - killed_children)
+		return 0;
+	current_leader = leader;
 	return 1;
 }
 
@@ -122,27 +158,25 @@ static void process_line(char *line, int idx, float ts)
 		if(REG_NOMATCH != retval)
 			errx(EXIT_FAILURE, "%s", "regexec failed");
 	} else {
-		fprintf(stderr, "*** new round line\n");
 		if(start_time < 0) {
 			start_time = ts;
-			memset(leaders, -1, sizeof(int) * PROC_NUM);
+			clear_leaders();
 			fprintf(stderr, "*** Registered new round\n");
 		}
 		return;
 	}
 
-	retval = regexec(&rx_leader, line, 2, match, 0);
+	retval = regexec(&rx_leader, line, 3, match, 0);
 	if(retval) {
 		if(REG_NOMATCH != retval)
 			errx(EXIT_FAILURE, "%s", "regexec failed");
 	} else {
-		line[match[0].rm_eo + 1] = '\0';
 		line[match[1].rm_eo + 1] = '\0';
-		round = atoi(line + match[0].rm_so);
-		leader = atoi(line + match[1].rm_so);
-		fprintf(stderr, "*** Round %d, Leader %d\n", round, leader);
+		line[match[2].rm_eo + 1] = '\0';
+		round = atoi(line + match[1].rm_so);
+		leader = atoi(line + match[2].rm_so);
 		if(round > current_round) {
-			memset(leaders, -1, sizeof(int) * PROC_NUM);
+			clear_leaders();
 			current_round = round;
 		}
 		leaders[idx] = leader;
@@ -150,10 +184,10 @@ static void process_line(char *line, int idx, float ts)
 			d = ts - start_time;
 			utarray_push_back(measurements, &d);
 			start_time = -1;
-			fprintf(stderr, "*** Leader elected, time %f ms\n", d);
+			fprintf(stderr, "*** Leader elected, time %f ms, round %d, leader %d\n", d, round, leader);
 		}
 	}
-
+	dump_leaders();
 }
 
 static void pipe_read_cb(EV_P_ ev_io *w, int revents)
@@ -203,28 +237,99 @@ static void pipe_read_cb(EV_P_ ev_io *w, int revents)
 	}
 }
 
-// another callback, this time for a time-out
-// static void timeout_cb (EV_P_ ev_timer *w, int revents)
-// {
-// }
+static void spawn_child(struct child_proc_item *child)
+{
+	char *mersenne_exe = "./mersenne";
+	char buf[BUF_SIZE];
+	char *argv[3] = {mersenne_exe, buf, NULL};
+	struct child_proc_item *tmp;
+	
+	HASH_FIND_INT(children, &child->cpi_fd, tmp);
+	if(NULL != tmp)
+		HASH_DEL(children, child);
 
-void init_child_proc_item(struct child_proc_item *item)
+	snprintf(buf, BUF_SIZE, "%d", child->cpi_index);
+	child->cpi_pid = forkpty(&child->cpi_fd, NULL, NULL, NULL);
+	make_fd_blocking(child->cpi_fd);
+	if(-1 == child->cpi_pid)
+		err(EXIT_FAILURE, "%s", "forkpty");
+	if(child->cpi_pid == 0) {
+		if(-1 == execv(mersenne_exe, argv))
+			err(EXIT_FAILURE, "%s", "exec failed");
+	}
+	ev_io_init(&child->cpi_io, pipe_read_cb, child->cpi_fd, EV_READ);
+	ev_io_start(loop, &child->cpi_io);
+
+	HASH_ADD_INT(children, cpi_fd, child);
+}
+
+static void kill_child(struct child_proc_item *child)
+{
+	if(0 == child->cpi_pid)
+		return;
+	ev_io_stop(loop, &child->cpi_io);
+	kill(child->cpi_pid, SIGTERM);
+	child->cpi_pid = 0;
+}
+
+static void sim_timeout_cb (EV_P_ ev_timer *w, int revents)
+{
+	struct child_proc_item *i, *tmp;
+	HASH_ITER(hh, children, i, tmp) {
+		kill_child(i);
+		HASH_DEL(children,i);
+		free(i);
+	}
+	ev_break(EV_A_ EVBREAK_ALL);
+}
+
+static void kill_timeout_cb (EV_P_ ev_timer *w, int revents)
+{
+	struct child_proc_item *c;
+
+	if(current_leader < 0)
+		return;
+
+	for(c=children; c != NULL; c=c->hh.next) {
+		if(0 == c->cpi_pid) {
+			spawn_child(c);
+			killed_children--;
+		} else if(current_leader == c->cpi_index) {
+			kill_child(c);
+			killed_children++;
+		}
+	}
+}
+
+static void init_child_proc_item(struct child_proc_item *item)
 {
 	item->cpi_buf_ptr = item->cpi_buf;
 }
 
+static void process_statistic()
+{
+	double *d;
+	for(d=(double*)utarray_front(measurements);
+			d!=NULL;
+			d=(double*)utarray_next(measurements,d)) {
+		printf("%f\n", *d);
+	}
+}
+
+
 int main()
 {
 	int proc_num = PROC_NUM;
-	char *mersenne_exe = "./mersenne";
-	char buf[BUF_SIZE];
-	char *argv[3] = {mersenne_exe, buf, NULL};
 	struct child_proc_item *child;
 	int i;
 
+	utarray_new(measurements,&double_icd);
 	// use the default event loop unless you have special needs
 	loop = EV_DEFAULT;
 	leaders = (int *)calloc(proc_num, sizeof(int));
+	clear_leaders();
+	killed_children = 0;
+	current_leader = PROC_LEADER_UNDEFINED;
 	if(regcomp (&rx_new_round, "^R [[:digit:]]: new round started", 0))
 		err(EXIT_FAILURE, "%s", "regcomp failed");
 	if(regcomp (&rx_leader, "^R \\([[:digit:]]\\+\\): Leader=\\([[:digit:]]\\+\\)", 0))
@@ -233,33 +338,26 @@ int main()
 	utarray_new(measurements, &double_icd);
 
 	for(i = 0; i < proc_num; i++) {
-		snprintf(buf, BUF_SIZE, "%d", i);
 		child = (struct child_proc_item *)
 			malloc(sizeof(struct child_proc_item));
 		init_child_proc_item(child);
 		child->cpi_index = i;
-		child->cpi_pid = forkpty(&child->cpi_fd, NULL, NULL, NULL);
-		make_fd_blocking(child->cpi_fd);
-		if(-1 == child->cpi_pid)
-			err(EXIT_FAILURE, "%s", "forkpty");
-		if(child->cpi_pid == 0) {
-			if(-1 == execv(mersenne_exe, argv))
-				err(EXIT_FAILURE, "%s", "exec failed");
-		}
-		ev_io_init(&child->cpi_io, pipe_read_cb, child->cpi_fd, EV_READ);
-		ev_io_start(loop, &child->cpi_io);
-		HASH_ADD_INT(children, cpi_fd, child);
+		spawn_child(child);
 	}
 
-	// initialise a timer watcher, then start it
-	// simple non-repeating 0.5 second timeout
-	// ev_timer_init(&kill_timer, timeout_cb, 5., 0.);
-	// ev_timer_start(loop, &kill_timer);
+	ev_timer_init(&sim_timer, sim_timeout_cb, 60., 0.);
+	ev_timer_start(loop, &sim_timer);
+	
+	ev_timer_init(&kill_timer, kill_timeout_cb, 1., 1.);
+	ev_timer_start(loop, &kill_timer);
 
-	// now wait for events to arrive
 	ev_loop(loop, 0);
 
 	printf("Simulation has finished\n");
+
+	process_statistic();
+
+	utarray_free(measurements);
 
 	// break was called, so exit
 	return 0;
