799c832ce290494c21d479cd4eda5826b66391f7
Konstantin Olkhovskiy <lupus@oxnull.net>
2013-03-08 23:32:58 +0400

    Yet another attempt to make learner better
    Introduced periodic hole checking. Currently it's not very smart and
    sends retransmit requests for anything that looks like a hole upon each
    run. Some age based approach should be exercised, i.e. when we swap the
    whole window and given that we are in sync with ``upstream'' we don't
    need to ask for retransmits straight away since it's likely that the
    system is currently working on this slice on instances and they will be
    filled soon.
    
    At the cost of some extra retransmits this learner seems to work better.

--- src/learner.c
+++ src/learner.c
@@ -117,16 +117,42 @@ static void send_retransmit(ME_P_ uint64_t from, uint64_t to)
 	pxs_send_acceptors(ME_A_ &msg);
 }
 
+static void retransmit_window(ME_P_ struct learner_context *context)
+{
+	uint64_t i, j;
+	uint64_t start = context->first_non_delivered;
+	uint64_t from = 0;
+	struct lea_instance *instance;
+	int collecting_gap = 0;
+	for (i = start, j = 0; j < LEA_INSTANCE_WINDOW; i++, j++) {
+		if (i == context->highest_seen) {
+			if (collecting_gap)
+				send_retransmit(ME_A_ from, i - 1);
+			break;
+		}
+		instance = get_instance(context, i);
+		if (collecting_gap) {
+			if (instance->closed) {
+				send_retransmit(ME_A_ from, i - 1);
+				collecting_gap = 0;
+			}
+		} else if(!instance->closed) {
+			from = i;
+			collecting_gap = 1;
+		}
+	}
+}
+
 static void retransmit_next_window(ME_P_ struct learner_context *context)
 {
 	context->next_retransmit = min(
-		context->first_non_delivered + LEA_INSTANCE_WINDOW,
-		context->highest_seen);
+			context->first_non_delivered + LEA_INSTANCE_WINDOW,
+			context->highest_seen);
 	send_retransmit(ME_A_ context->first_non_delivered,
 			min(context->highest_seen, context->next_retransmit));
 	fbr_log_d(&mctx->fbr, "requesting retransmits from %lu to %lu, highest seen is %lu",
-		context->first_non_delivered, context->next_retransmit,
-		context->highest_seen);
+			context->first_non_delivered, context->next_retransmit,
+			context->highest_seen);
 }
 
 static void try_deliver(ME_P_ struct learner_context *context)
@@ -134,9 +160,9 @@ static void try_deliver(ME_P_ struct learner_context *context)
 	int i, j;
 	int start = context->first_non_delivered;
 	struct lea_instance *instance;
-	for(j = 0, i = start; j < LEA_INSTANCE_WINDOW; j++, i++) {
+	for (j = 0, i = start; j < LEA_INSTANCE_WINDOW; j++, i++) {
 		instance = get_instance(context, i);
-		if(!instance->closed)
+		if (!instance->closed)
 			return;
 		do_deliver(ME_A_ context, instance);
 		context->first_non_delivered = i + 1;
@@ -145,7 +171,7 @@ static void try_deliver(ME_P_ struct learner_context *context)
 		bm_init(instance->acks, peer_count(ME_A));
 		sm_free(instance->v);
 		instance->v = NULL;
-		if(context->first_non_delivered == context->next_retransmit)
+		if (context->first_non_delivered == context->next_retransmit)
 			retransmit_next_window(ME_A_ context);
 	}
 }
@@ -160,10 +186,10 @@ static void do_learn(ME_P_ struct learner_context *context, struct
 
 	data = &pmsg->data.me_paxos_msg_data_u.learn;
 
-	if(data->i < context->first_non_delivered)
+	if (data->i < context->first_non_delivered)
 		return;
-	if(data->i >= context->first_non_delivered + LEA_INSTANCE_WINDOW) {
-		if(data->i > context->highest_seen)
+	if (data->i >= context->first_non_delivered + LEA_INSTANCE_WINDOW) {
+		if (data->i > context->highest_seen)
 			context->highest_seen = data->i;
 		fbr_log_d(&mctx->fbr, "value out of instance windows, discarding");
 		fbr_log_d(&mctx->fbr, "data->i == %lu while "
@@ -173,9 +199,9 @@ static void do_learn(ME_P_ struct learner_context *context, struct
 	}
 
 	instance = get_instance(context, data->i);
-	if(instance->closed)
+	if (instance->closed)
 		return;
-	if(NULL == instance->v) {
+	if (NULL == instance->v) {
 		instance->iid = data->i;
 		instance->b = data->b;
 		assert(buf->size1 > 0);
@@ -188,7 +214,7 @@ static void do_learn(ME_P_ struct learner_context *context, struct
 	}
 	bm_set_bit(instance->acks, from->index, 1);
 	num = bm_hweight(instance->acks);
-	if(pxs_is_acc_majority(ME_A_ num)) {
+	if (pxs_is_acc_majority(ME_A_ num)) {
 		instance->closed = 1;
 		try_deliver(ME_A_ context);
 	}
@@ -200,12 +226,35 @@ static void do_last_accepted(ME_P_ struct learner_context *context, struct
 	struct me_paxos_last_accepted_data *data;
 
 	data = &pmsg->data.me_paxos_msg_data_u.last_accepted;
-	if(data->i < context->first_non_delivered)
-		return;
-	if(data->i > context->first_non_delivered) {
-		if(data->i > context->highest_seen)
-			context->highest_seen = data->i;
-		retransmit_next_window(ME_A_ context);
+	if (data->i > context->highest_seen)
+		context->highest_seen = data->i;
+}
+
+static void lea_hole_checker_fiber(struct fbr_context *fiber_context,
+		void *_arg)
+{
+	struct me_context *mctx;
+	struct learner_context *context = _arg;
+
+	mctx = container_of(fiber_context, struct me_context, fbr);
+
+	for (;;) {
+		fbr_sleep(&mctx->fbr, 0.5);
+		if (context->highest_seen > context->first_non_delivered +
+				LEA_INSTANCE_WINDOW) {
+			fbr_log_i(&mctx->fbr, "learner is lagging behind,"
+					" highest seen: %zu, highest delivered:"
+					" %zu", context->highest_seen,
+					context->first_non_delivered - 1);
+			retransmit_window(ME_A_ context);
+			retransmit_next_window(ME_A_ context);
+		} else if(context->highest_seen > context->first_non_delivered) {
+			fbr_log_i(&mctx->fbr, "learner is out of sync, highest"
+					" closed: %zu, highest delivered: %zu",
+					context->highest_seen,
+					context->first_non_delivered - 1);
+			retransmit_window(ME_A_ context);
+		}
 	}
 }
 
@@ -238,7 +287,7 @@ static struct learner_context *init_context(ME_P_ struct lea_fiber_arg *arg)
 
 	context->item.id = fbr_self(&mctx->fbr);
 	TAILQ_INSERT_TAIL(&mctx->learners, &context->item, entries);
-	context->next_retransmit = ~0UL; // "infinity"
+	context->next_retransmit = ~0ULL; // "infinity"
 	context->instances = fbr_alloc(&mctx->fbr, LEA_INSTANCE_WINDOW *
 			sizeof(struct lea_instance));
 	context->highest_seen = context->first_non_delivered;
@@ -262,12 +311,17 @@ void lea_fiber(struct fbr_context *fiber_context, void *_arg)
 	struct learner_context *context;
 	struct fbr_buffer *fb;
 	struct msg_info *info;
+	fbr_id_t hole_checker;
 
 	mctx = container_of(fiber_context, struct me_context, fbr);
 	context = init_context(ME_A_ _arg);
 	fb = context->buffer;
 
-	for(;;) {
+	hole_checker = fbr_create(&mctx->fbr, "learner/hole_checker",
+			lea_hole_checker_fiber, context, 0);
+	fbr_transfer(&mctx->fbr, hole_checker);
+
+	for (;;) {
 		info = fbr_buffer_read_address(&mctx->fbr, fb,
 				sizeof(struct msg_info));
 
