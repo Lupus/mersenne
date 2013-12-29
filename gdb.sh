3a29a8c0bcc0387278819c2a8db426ea57feb441
Konstantin Olkhovskiy <kolkhovskiy@nvidia.com>
2013-12-29 15:28:42 +0400

    Changed gdb.sh to run cgdb

--- gdb.sh
+++ gdb.sh
@@ -3,6 +3,4 @@ NUM=$1
 if [ ! -d socks ] ; then
 	mkdir socks
 fi
-echo -e "set args -s ./socks/$NUM -p $NUM --acceptor-wal-dir "acceptor$NUM"" > gdb.init
-gdb -x gdb.init ./build/mersenne
-rm gdb.init
+cgdb -- --args ./build/mersenne -s ./socks/$NUM -p $NUM --acceptor-wal-dir "acceptor$NUM"
