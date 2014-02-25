5a78613f12e2ec9ee8afad0e61cd2c7882fa1085
Konstantin Olkhovskiy <kolkhovskiy@nvidia.com>
2014-02-25 17:02:17 +0400

    Support for AddressSanitizer

--- build.sh
+++ build.sh
@@ -9,32 +9,24 @@ pushd build
 
 case $1 in
 	clang)
-		export LD=/usr/bin/clang
-		export CC=/usr/bin/clang
-		cmake -DCMAKE_BUILD_TYPE=Debug ..
+		export LD=/usr/local/bin/clang
+		export CC=/usr/local/bin/clang
+		cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_ASAN=TRUE ..
 		;;
 	clang-release)
-		export LD=/usr/bin/clang
-		export CC=/usr/bin/clang
+		export LD=/usr/local/bin/clang
+		export CC=/usr/local/bin/clang
 		cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 		;;
 	gcc)
 		export LD=/usr/bin/gcc
 		export CC=/usr/bin/gcc
-		cmake -DCMAKE_BUILD_TYPE=Debug ..
+		cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_ASAN=TRUE -DLINK_ASAN_LIB=TRUE ..
 		;;
 	gcc-release)
 		export LD=/usr/bin/gcc
 		export CC=/usr/bin/gcc
 		cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
-		;;
-	libevfibers)
-		export LD=/usr/bin/clang
-		export CC=/usr/bin/clang
-		cmake -DCMAKE_BUILD_TYPE=Debug \
-			-DLIBEVFIBERS_INCLUDE_PATH=~/git/libevfibers/include \
-			-DLIBEVFIBERS_LIBDIR=~/git/libevfibers/build \
-			..
 		;;
 	*)
 		echo "Please enter valid build type"
