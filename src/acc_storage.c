275a1060f7bef3b68c8e61fe4e6ff7b48fd31209
Konstantin Olkhovskiy <kolkhovskiy@nvidia.com>
2014-04-18 12:44:14 +0400

    Run vacuum only if threshold is exceeded

--- src/acc_storage.c
+++ src/acc_storage.c
@@ -1042,14 +1042,15 @@ void acs_vacuum(ME_P)
 {
 	struct acs_context *ctx = &mctx->pxs.acc.acs;
 	struct acc_instance_record *r, *x;
-	int trunc;
+	int trunc, threshold;
 	trunc = mctx->args_info.acceptor_truncate_arg;
+	threshold = trunc * (1 + mctx->args_info.acceptor_truncate_margin_arg);
 
 	/*
 	printf("%ld <= %d == %d\n", ctx->highest_finalized, trunc,
 			ctx->highest_finalized <= trunc);
 			*/
-	if (ctx->highest_finalized <= trunc)
+	if (HASH_COUNT(ctx->instances) <= threshold)
 			return;
 	HASH_ITER(hh, ctx->instances, r, x) {
 		if (r->iid >= ctx->highest_finalized - trunc)
