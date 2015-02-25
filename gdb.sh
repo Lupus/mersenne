3c5bb5b0e41c84d1fa5fa93ba4656a823e0138be
Konstantin Olkhovskiy <kolkhovskiy@nvidia.com>
2015-02-25 14:26:45 +0300

    Fixed gdb launch script

--- gdb.sh
+++ gdb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 NUM=$1
-cgdb -- --args ./build/mersenne -p $NUM --acceptor-wal-dir "acceptor$NUM"
+mkdir -p acceptor$NUM/{snap,wal}
+cgdb -- --args ./build/mersenne -p $NUM --acceptor-wal-dir "acceptor$NUM/wal" --acceptor-snap-dir "acceptor$NUM/snap"
