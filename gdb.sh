75db964ea1026318d3e510e9154c5f29339736f8
Konstantin Olkhovskiy <kolkhovskiy@nvidia.com>
2015-07-16 12:10:32 +0300

    Fixed gdb launch script

--- gdb.sh
+++ gdb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NUM=$1
 mkdir -p acceptor$NUM/db
-cgdb -- --args ./build/mersenne -p $NUM --acceptor-db-dir "acceptor$NUM/db"
+cgdb -- --args ./build/mersenne -p $NUM --db-dir "state$NUM"
